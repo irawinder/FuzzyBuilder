e5d3ecae60b4b1de3e099996adab2e32de680665
irawinder <jamesirawinder@gmail.com>
2021-12-06 16:32:48 +0900

    Parameterize Server Name

--- Eclipse/fuzzy-io/src/edu/mit/ira/fuzzy/FuzzyIO.java
+++ Eclipse/fuzzy-io/src/edu/mit/ira/fuzzy/FuzzyIO.java
@@ -4,10 +4,11 @@ import edu.mit.ira.fuzzy.io.Server;
 
 public class FuzzyIO {
 	
+	private static final String NAME = "FuzzyIO";
 	private static final String VERSION = "v1.1.2";
 	private static final int PORT = 8080;
 	
 	public static void main(String[] args) throws Exception {
-		new Server(VERSION, PORT);
+		new Server(NAME, VERSION, PORT);
 	}
 }
