ab834a8b035d678c57efcbef2a3697cc4da5e850
irawinder <jamesirawinder@gmail.com>
2021-12-19 03:41:38 +0900

    Include Legend in modded Config

--- Eclipse/fuzzy-io/src/edu/mit/ira/fuzzy/setting/Deserializer.java
+++ Eclipse/fuzzy-io/src/edu/mit/ira/fuzzy/setting/Deserializer.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import edu.mit.ira.fuzzy.model.Function;
+import edu.mit.ira.fuzzy.setting.Legend.Entry;
+
 /**
  * A Utility Class to deserialize setting data
  *
@@ -22,19 +25,31 @@ public class Deserializer {
 	public Configuration parse(String data) {
 		JSONObject configJSON = new JSONObject(data);
 		try {
+			
+			// Header
 			String label = configJSON.getString("label");
 			String apiVersion = configJSON.getString("apiVersion");
 			String id = configJSON.getString("id");
 			String author = configJSON.getString("author");
 			String contact = configJSON.getString("contact");
 			String sponsor = configJSON.getString("sponsor");
+			
+			// Init config
 			Configuration config = new Configuration(label, apiVersion, id, author, sponsor, contact);
+			
+			// Settings
 			JSONArray settingsJSON = configJSON.getJSONArray("settings");
 			for(int i=0; i<settingsJSON.length(); i++) {
 				JSONObject settingJSON = settingsJSON.getJSONObject(i);
 				config.settings.add(parseSetting(settingJSON));
 			}
+			
+			// Legend
+			JSONObject legendJSON = configJSON.getJSONObject("legend");
+			config.legend = parseLegend(legendJSON);
+			
 			return config;
+			
 		} catch (Exception e) {
 			System.out.println("JSON data is not formatted correctly");
 			return null;
@@ -85,5 +100,19 @@ public class Deserializer {
 			}
 		}
 		return setting;
+	}
+	
+	private Legend parseLegend(JSONObject legendJSON) {
+		Legend legend = new Legend();
+		legend.label = legendJSON.getString("label");
+		JSONArray entriesJSON = legendJSON.getJSONArray("entries");
+		for(int i=0; i<entriesJSON.length(); i++) {
+			JSONObject entryJSON = entriesJSON.getJSONObject(i);
+			Entry entry = legend.new Entry();
+			entry.label = entryJSON.getString("label");
+			entry.color = entryJSON.getString("color");
+			legend.entries.add(entry);
+		}
+		return legend;
 	}
 }
