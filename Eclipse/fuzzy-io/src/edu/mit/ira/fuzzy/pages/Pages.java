18797d05bf8347addec82ce0008e05fdab79e07b
Ira Winder <jamesirawinder@gmail.com>
2022-09-03 19:02:18 +0900

    Added Javascript Page to input email address

--- Eclipse/fuzzy-io/src/edu/mit/ira/fuzzy/pages/Pages.java
+++ Eclipse/fuzzy-io/src/edu/mit/ira/fuzzy/pages/Pages.java
@@ -48,13 +48,36 @@ public class Pages {
 		head += "<style>";
 
 		String imgUrl = "https://github.com/irawinder/FuzzyBuilder/blob/master/screenshots/massing.png?raw=true";
-
-		head += "body {margin-bottom: 60px;margin-top: 40px;max-width: 800px;background-color:#ffffff;background-image:url(" + imgUrl + ");background-repeat:no-repeat;background-position:center center;background-attachment:fixed;}";
-		head += "h1 {text-align:left;margin-left: 40px;margin-right: 40px;font-family:Helvetica, sans-serif;color:#696969;}";
-		head += "h2 {text-align:left;margin-left: 40px;margin-right: 40px;font-family:Helvetica, sans-serif;color:#696969;}";
-		head += "h3 {text-align:left;margin-left: 40px;margin-right: 40px;font-family:Helvetica, sans-serif;color:#696969;}";
-		head += "p {text-align:left;margin-left: 40px;margin-right: 40px;font-family:Helvetica, sans-serif;font-size:14px;font-style:normal;font-weight:normal;color:#525252;}";
-		head += "li {text-align:left;margin-left: 40px;margin-right: 40px;font-family:Helvetica, sans-serif;font-size:14px;font-style:normal;font-weight:normal;color:#525252;}";
+		
+		String generalCSS = "text-align:left;"
+				+ "margin-left: 40px;"
+				+ "margin-right: 40px;"
+				+ "font-family:Helvetica, sans-serif;";
+		
+		String bodyTextCSS = "font-size:14px;"
+				+ "font-style:normal;"
+				+ "font-weight:normal;"
+				+ "color:#525252;";
+		
+		head += "body {"
+				+ "margin-bottom: 60px;"
+				+ "margin-top: 40px;"
+				+ "max-width: 800px;"
+				+ "background-color:#ffffff;"
+				+ "background-image:url(" + imgUrl + ");"
+				+ "background-repeat:no-repeat;"
+				+ "background-position:center center;"
+				+ "background-attachment:fixed;"
+				+ "}";
+		
+		head += "h1 { " + generalCSS + "color:#696969; }";
+		head += "h2 { " + generalCSS + "color:#696969; }";
+		head += "h3 { " + generalCSS + "color:#696969; }";
+		head += "p { " + generalCSS + bodyTextCSS + " }";
+		head += "label { " + generalCSS + bodyTextCSS + " }";
+		head += "input { " + generalCSS + bodyTextCSS + " }";
+		head += "button { " + generalCSS + bodyTextCSS + " }";
+		head += "li {" + generalCSS + bodyTextCSS + "}";
 		head += "ul li { margin-bottom: 10px; }";
 		head += "ol li { margin-bottom: 10px; }";
 
@@ -106,9 +129,78 @@ public class Pages {
 	private static String makeRegistrationBody() {
 
 		String body = "<body>";
-
+		
+		body += "<script>";
+		
+		body += "function register() {"
+				
+				+ "document.getElementById(\"feedback\").innerHTML = \"\";"
+				+ "document.getElementById(\"email1_feedback\").innerHTML = \"\";"
+				+ "document.getElementById(\"email2_feedback\").innerHTML = \"\";"
+				
+				+ "var email1 = document.getElementById(\"email1\").value;"
+				+ "var email2 = document.getElementById(\"email2\").value;"
+				
+				+ "var valid1 = false;"
+				+ "if (email1.split(\"@\").length == 2)"
+				+ "{"
+				+ "if (email1.split(\"@\")[1].split(\".\").length == 2)"
+				+ "{"
+				+ "valid1 = true;"
+				+ "}"
+				+ "}"
+				
+				+ "var valid2 = false;"
+				+ "if (email2.split(\"@\").length == 2)"
+				+ "{"
+				+ "if (email2.split(\"@\")[1].split(\".\").length == 2)"
+				+ "{"
+				+ "valid2 = true;"
+				+ "}"
+				+ "}"
+				
+				+ "if (!valid1)"
+				+ "{"
+				+ "document.getElementById(\"email1_feedback\").innerHTML = \"you must enter a valid email address\";"
+				+ "}"
+				+ "if (!valid2)"
+				+ "{"
+				+ "document.getElementById(\"email2_feedback\").innerHTML = \"you must enter a valid email address\";"
+				+ "}"
+				
+				+ "if (email1.length == 0)"
+				+ "{"
+				+ "document.getElementById(\"feedback\").innerHTML = \"you must enter a valid email address\";"
+				+ "}"
+				+ "else if (email1 != email2)"
+				+ "{"
+				+ "document.getElementById(\"feedback\").innerHTML = \"email addresses must match\";"
+				+ "}"
+				+ "else"
+				+ "{"
+				+ "var email = email1.replace(\"@\", \"%40\");"
+				+ "email = email.replace(\".\", \"%2E\");"
+				+ "var url = \"/register?email=\" + email;"
+				+ "location.replace(url); "
+				+ "}"
+				
+				+ "}";
+		
+		body += "</script>";
+		
 		body += wrapText("h1", "User Registration");
-		body += wrapText("p", "Placeholder Text");
+		
+		body += "<label for=\"email1\">Please enter your email address:</label><br>";
+		
+		body += "<input type=\"text\" id=\"email1\" style=\"width: 300px;\"><p style=\"color: red;\" id=\"email1_feedback\"></p><br><br>";
+		
+		body += "<label for=\"email2\">Please enter your email address again:</label><br>";
+		
+		body += "<input type=\"text\" id=\"email2\" style=\"width: 300px;\"><p style=\"color: red;\" id=\"email2_feedback\"></p><br><br>";
+		
+		body += "<button onclick=\"register()\">Register</button>";
+		
+		body += "<p style=\"color: red;\" id=\"feedback\"></p>";
 
 		body += "</body>";
 
