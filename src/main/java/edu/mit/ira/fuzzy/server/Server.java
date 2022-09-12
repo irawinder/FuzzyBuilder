9b99608235be71b803c63a5eaa360e3504ab7bdf
Ira Winder <jamesirawinder@gmail.com>
2022-09-12 21:55:28 +0900

    Update jar to v1.5.0

--- src/main/java/edu/mit/ira/fuzzy/server/Server.java
+++ src/main/java/edu/mit/ira/fuzzy/server/Server.java
@@ -50,7 +50,7 @@ import edu.mit.ira.opensui.setting.Configuration;
 public class Server {
 	
 	public static final String NAME = "FuzzyIO";
-	public static final String VERSION = "v1.4.22";
+	public static final String VERSION = "v1.5.0";
 	public static final String AUTHOR = "Ira Winder, Daniel Fink, and Max Walker";
 	public static final String SPONSOR = "MIT Center for Real Estate";
 	public static final String CONTACT = "fuzzy-io@mit.edu";
